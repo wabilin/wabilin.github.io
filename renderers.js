945c5d5a0c6844e4f5de5a5ca75cb6473efe13dc
wabilin <twelendil@protonmail.com>
2020-06-22 23:47:09 +0900

    basic style

--- renderers.js
+++ renderers.js
@@ -13,4 +13,27 @@ const metadata = {
   categories: [],
 }
 
-module.exports = { metadata }
+const post = ({ title, link }) => (`
+<header class="post-header">
+  <h2>
+    <a href="${link}">${ title }</a>
+  </h2>
+</header>
+`);
+
+const index = posts => {
+  const f = posts[0]
+  console.log(JSON.stringify(f))
+  const sorted = posts
+    .sort((a, b) => a.link.localeCompare(b.link) * -1)
+    .map(post)
+    .join('')
+
+  return sorted;
+}
+
+const head = (document) => {
+  document.title = 'Explosion!'
+}
+
+module.exports = { metadata, index, head }
