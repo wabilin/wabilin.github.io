fac4d328d0c63d50a7e5ed808ca741f95e455e50
wabilin <twelendil@protonmail.com>
2020-06-24 01:14:11 +0900

    add metas

--- renderers.js
+++ renderers.js
@@ -20,7 +20,7 @@ const post = ({ title, link }) => {
 <header class="post-header">
   <h3 class="post-time">${timeStr}</h3>
   <h2>
-    <a href="${link}">${ title }</a>
+    <a href="${link}">${title}</a>
   </h2>
 </header>
 `
@@ -37,8 +37,34 @@ const index = posts => {
   return sorted;
 }
 
-const head = (document) => {
+const head = (document, data) => {
   document.title = 'Explosion!'
+
+  if (!data)
+    return
+
+  const meta = (name, content) => {
+    const meta = document.createElement('meta')
+    meta.setAttribute('property', name)
+    meta.setAttribute('content', content)
+    document.head.appendChild(meta)
+  }
+
+  meta('og:type', 'article')
+  if ('title' in data) {
+    document.title += ` | ${data.title}`
+    meta('og:title', data.title)
+  }
+
+  if ('link' in data) {
+    const dateMatch = data.link.match(/\d\d\d\d-\d\d-\d\d/)
+    const date = dateMatch && dateMatch[0]
+    meta('article:published_time', new Date(date).toISOString())
+  }
+
+  if ('tags' in data) {
+    meta('article:tag', data.tags)
+  }
 }
 
 module.exports = { metadata, index, head }
